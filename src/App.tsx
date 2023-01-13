bd9732b5419c372dbf9dc6f614ce3141f211b4d6
sameermanjrekaragilisium <sameer.manjrekar@agilisium.com>
2023-01-13 10:20:22 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>Sameer</div>;
+}
+
+export default App;
---
+++ src/components/Sidenav.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Sidenav = (props: Props) => {
+  return (
+    <div>Sidenav</div>
+  )
+}
+
+export default Sidenav
